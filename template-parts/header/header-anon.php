bb5c94adef68307e90beb47e03bd7d104b6b66eb
Easyteam.org SRL <info@easyteam.org>
2022-12-15 22:14:56 +0100

    Update header-anon.php
    Allineato aria-label con tema parent

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -1,5 +1,5 @@
 <!-- ### INIZIO MODIFICA EASYTEAM.ORG ### -->
-<button type="button" class="icon-text" aria-label="UserGoogle">
+<button type="button" class="icon-text" aria-label="<?php _e("Accedi con Google", "design_scuole_italia"); ?>">
         <span class="mr-2 d-none d-lg-block"><a href="#googleworkspace"><?php _e("Accedi con Google", "design_scuole_italia"); ?></a></span>
     <div class="icon-wrapper">
         <svg class="svg-user"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-user"></use></svg>
@@ -7,7 +7,7 @@
     </button>
 <!-- ### FINE MODIFICA EASYTEAM.ORG ### -->
 
-<button type="button" class="icon-text" data-toggle="modal" data-target="#access-modal" aria-label="User">
+<button type="button" class="icon-text" data-toggle="modal" data-target="#access-modal" aria-label="<?php _e("Accedi", "design_scuole_italia"); ?>">
     <span class="mr-2 d-none d-lg-block"><?php _e("Accedi", "design_scuole_italia"); ?></span>
     <div class="icon-wrapper">
         <svg class="svg-user"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-user"></use></svg>
