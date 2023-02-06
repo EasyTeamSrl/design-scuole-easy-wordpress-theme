a5475bfa7c3a70e029d3b694d4d9cad91c629fe5
Easyteam.org SRL <info@easyteam.org>
2023-02-06 18:40:18 +0100

    Agid v. 2.1.8
    Correzione errore ALT in avatar

--- functions.php
+++ functions.php
@@ -1,5 +1,18 @@
 <?php
 
+// Add alt tag to WordPress Gravatar images
+function bloggerpilot_gravatar_alt($bloggerpilotGravatar) {
+	if (have_comments()) {
+		$alt = get_comment_author();
+	}
+	else {
+		$alt = get_the_author_meta('display_name');
+	}
+	$bloggerpilotGravatar = str_replace('alt=\'\'', 'alt=\'Avatar f&uuml;r ' . $alt . '\'', $bloggerpilotGravatar);
+	return $bloggerpilotGravatar;
+}
+add_filter('get_avatar', 'bloggerpilot_gravatar_alt');
+
 // Includo i fogli di stile del tema padre
 add_action( 'wp_enqueue_scripts',
 'enqueue_parent_theme_style' );
